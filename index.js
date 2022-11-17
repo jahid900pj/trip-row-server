80ee7e435ecf1e77fa45ebea50e4b8109b026d4c
jahidhasan234031 <jahidhasan234031@mail.com>
2022-11-17 09:02:34 +0600

    all done

--- index.js
+++ index.js
@@ -86,12 +86,12 @@ async function tripConnected() {
 
         })
 
-        // app.delete('/reviews/:id', async (req, res) => {
-        //     const id = req.params.id
-        //     const query = { _id: ObjectId(id) }
-        //     const result = await reviewCollection.deleteOne(query)
-        //     res.send(result)
-        // })
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally {
 
